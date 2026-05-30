e65d4398011bc7f1c0a87d77294de8ef5b68f812
Peter Bellaby <bellaby1993@hotmail.co.uk>
2026-05-30 16:43:06 +0100

    added deployment pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
-cd ~/TrinitasApi
-
-git fetch origin main
-
-if ! git diff --quiet HEAD origin/main -- package.json; then
-    echo "Changes detected in package.json. Running npm install..."
-    git pull origin main
-    npm install --production
-else
-    echo "No changes in package.json. Skipping npm install."
-    git pull origin main
+if [ ! -d ~/NodeChatApp ]; then
+  git clone https://github.com/Tamillis/NodeChatApp.git ~/NodeChatApp
 fi
 
-pm2 reload trinitas-api --update-env
+cd ~/NodeChatApp
+
+git fetch origin main
+git reset --hard origin/main
+
+cd ~/NodeChatApp/NodeChatClient
+npm install
+npm run build
+
+rm -rf /var/www/chat-site/assets/*
+rm -f  /var/www/chat-site/index.html
+cp -r dist/* /var/www/chat-site/
+
+cd ~/NodeChatApp/NodeChatServer
+
+if pm2 describe ncs-ws > /dev/null 2>&1; then
+  pm2 reload ncs-ws --update-env
+else
+  PORT=3005 pm2 start ~/NodeChatApp/NodeChatServer/ncsMain.js --name ncs-ws
+  pm2 save
+fi
